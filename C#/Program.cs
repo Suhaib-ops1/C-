192d413c11be15ef5bdaf113095e1299628e22ac
Suhaib-ops1 <suhaipabulail@gmail.com>
2025-01-16 16:18:00 +0300

    Add project files.

---
+++ C#/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace firsttask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"read value is: {name}");
+
+            double q = 3.45;
+            Console.WriteLine(q);
+
+
+            string w = "suhaib";
+            Console.WriteLine(w);
+
+            char e = 'a';
+            Console.WriteLine(e);
+
+            bool r = true;
+            Console.WriteLine(r);
+
+            int t = 9;
+            Console.WriteLine(t);
+
+            const int number = 11;
+            Console.WriteLine (number);
+        }
+    }
+}
