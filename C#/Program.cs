0c9aeda96b1dcf3d71278c098a735dbb51fd5003
Suhaib-ops1 <suhaipabulail@gmail.com>
2025-01-16 17:01:49 +0300

    solved the task

--- C#/Program.cs
+++ C#/Program.cs
@@ -30,6 +30,38 @@ namespace firsttask
             Console.WriteLine (number);
 
             string[] cars = { "BMW", "TOYOTA", "HYNDAI", "MASARATI", "AUDI" };
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+            string first_name = Console.ReadLine();
+            Console.WriteLine($"Your first name is: {first_name}");
+
+            string last_name= Console.ReadLine();
+            Console.WriteLine($"Your last name is: {last_name}");
+
+            string year_of_birth = Console.ReadLine();
+            Console.WriteLine($"Year of birth: {year_of_birth}");
+
+            Console.WriteLine($"{first_name} {last_name} {year_of_birth}");
+
+
+            string[] numbers= new string[10];
+            for (int i = 0; i < numbers.Length; i++) { 
+                
+                numbers[i] = Console.ReadLine();
+            }
+            //foreach (string car1 in numbers) 
+            //{
+            //    Console.WriteLine("the elements in array are: "+car1);
+            //}
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("the elements in array {0} are : {1} ",i ,numbers[i]);
+            }
+
         }
     }
 }
