ade3bde4a01ffe35543b5de0c7b8311092e0fc47
Suhaib-ops1 <suhaipabulail@gmail.com>
2025-01-21 17:33:35 +0300

    Solved task 3 and 4

---
+++ task3/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_20_1
+{
+    internal class Program
+    {
+        static string isOdd(int x)
+        {
+            if (x % 2 == 0)
+                return ($"{x} is even");
+
+            else
+            {
+                return ($" {x} is odd");
+            }
+        }
+
+        static int secSmall(int[] num)
+        {
+            Array.Sort(num);
+            return num[1];
+        }
+
+        static int factorial(int x)
+        {
+            if (x == 1)
+            {
+                return 1;
+            }
+            int f = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                f *= i;
+            }
+            return f;
+        }
+
+        static void Prims(int n1, int n2)
+        {
+
+            for (int i = n1; i <= n2; i++)
+            {
+                if (i == 2 || i == 3 || i == 5)
+                {
+                    Console.WriteLine(i);
+                }
+                if ((i % 2) == 0 || (i % 5 == 0) || (i % 3 == 0))
+                {
+                    continue;
+                }
+
+                int counter = 0;
+                for (int j = 2; j <= i; j++)
+                {
+                    if ((i % j) == 0)
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == 1)
+                {
+                    Console.WriteLine(i);
+
+                }
+
+            }
+        }
+
+        static int MaxValue(int[] numbers)
+        {
+            int max = numbers[0];
+            foreach (int n in numbers)
+            {
+                if (n > max)
+                {
+                    max = n;
+                }
+            }
+            return max;
+        }
+
+        static string calcSum(int[] num)
+        {
+            int sumE = 0, sumO = 0;
+
+            foreach (int n in num)
+            {
+                if (n % 2 == 0)
+                {
+                    sumE += n;
+                }
+                else
+                {
+                    sumO += n;
+                }
+
+            }
+            return ($"sum of even: {sumE} , sum of odd: {sumO}");
+        }
+
+        static void findCommon(int[] arr1, int[] arr2)
+        {
+            string common = "";
+            foreach (int n in arr1)
+            {
+                foreach (int n2 in arr2)
+                {
+                    if (n == n2)
+                    {
+                        common += n + ", ";
+                    }
+                }
+            }
+            Console.WriteLine(common);
+        }
+
+        class room
+        {
+            public int roomNum;
+            public string type;
+            public double price;
+            public bool isBooked;
+            public const string HotelName = "Grand Stay Hotel";
+        }
+        static void Main(string[] args)
+        {
+            //1-	Write a C# program that accepts an integer and determines if the number is even or odd. Display the result.
+
+            Console.WriteLine("enter number to check if it odd or even:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(isOdd(num));
+
+            ////2-	Write a C# program that accepts five numbers and displays the  2nd smallest of the five.
+
+            int[] arr = new int[5];
+            Console.WriteLine("enter five number to displays the  2nd smallest of the five :");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+            int num5 = int.Parse(Console.ReadLine());
+            arr[0] = num1;
+            arr[1] = num2;
+            arr[2] = num3;
+            arr[3] = num4;
+            arr[4] = num5;
+            Console.WriteLine("2nd smallest of the five: " + secSmall(arr));
+
+
+
+            //3-	Write a program that calculates the factorial of a given number using a for loop.
+
+            Console.WriteLine("enter number to calculates the factorial:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(factorial(number));
+
+            //4-	Write a program that takes two integers as input and prints all the prime numbers between them using nested loops.
+
+            Console.WriteLine("enter two integers as input and prints all the prime numbers between them using nested loops:");
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            Prims(number1, number2);
+
+
+            //5-	Create a program that initializes an integer array and finds the largest number in it using a foreach loop.
+
+            int[] numbers = { 1, 3, 4, 5, 6, 7, 8 };
+            Console.WriteLine("the largest number :" + MaxValue(numbers));
+
+            //6-	Write a program that prints the following number pattern using nested loops:
+            // For n = 5, the output should be:
+            int s = 0;
+
+            for (int i = 0; i <= 5; i++)
+            {
+                string Line = "";
+                for (int j = 1; j <= i; j++)
+                {
+                    s++;
+                    Line += Convert.ToString(s) + " ";
+                }
+                Console.WriteLine(Line);
+            }
+
+            //7-	Write a program that prints a pyramid pattern using stars (*) based on the input height. Use nested loops. (optional)
+
+            int c = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                string line = "";
+                for (int j = 0; j < i + c; j++)
+                {
+                    line += "* ";
+                }
+
+                Console.WriteLine(line);
+                c++;
+            }
+
+            // 8 - Write a program that takes an array of integers and calculates the sum of even numbers and odd numbers separately using loops.  (optional)
+
+            Console.WriteLine("calculate the sum of odd and even");
+            int[] arr1 = { 12, 4, 6, 8, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, };
+            Console.WriteLine(calcSum(arr1));
+
+            //Write a program that compares two arrays and prints their common elements using loops. (optional)
+
+            int[] a1 = { 1, 2, 3, 4 };
+            int[] a2 = { 3, 4, 5, 6 };
+
+            findCommon(a1, a2);
+
+            //----------------------------------------------------------
+            Console.WriteLine("enter two room number");
+            int roomid1 = int.Parse(Console.ReadLine());
+            int roomid2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter two room types");
+            string roomType1 = Console.ReadLine();
+            string roomType2 = Console.ReadLine();
+
+            Console.WriteLine("enter two room price");
+            double roomprice1 = double.Parse(Console.ReadLine());
+            double roomprice2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter two room avilability T/F");
+            bool roombool1 = Convert.ToBoolean(Console.ReadLine());
+            bool roombool2 = Convert.ToBoolean(Console.ReadLine());
+
+            room room1 = new room();
+            room1.roomNum = roomid1;
+            room1.type = roomType1;
+            room1.price = roomprice1;
+            room1.isBooked = roombool1;
+
+            room room2 = new room();
+            room2.roomNum = roomid2;
+            room2.type = roomType2;
+            room2.price = roomprice2;
+            room2.isBooked = roombool2;
+
+            Console.WriteLine($"room1 : {room1.roomNum}, {room1.type}, {room1.price}, {room1.isBooked}, {room.HotelName}");
+            Console.WriteLine($"room2 : {room2.roomNum}, {room2.type}, {room2.price}, {room2.isBooked}, {room.HotelName}");
+
+        }
+    }
+}
---
+++ task4/Program.cs
@@ -0,0 +1,119 @@
+﻿
+/* 1.	Create a Student Class:
+o	Create a class named Student with the following:
+	A property named Name of type string to represent the student's name.
+	A property named Age of type int to represent the student's age.
+	A property named StudentID of type int to represent the student's ID.
+	Add an auto property named Email to represent the student’s email.
+2.	Add Constants:
+o	Add a constant named MinAge to represent the minimum allowed age for a student (18 years).
+o	Add a constant named MaxAge to represent the maximum allowed age for a student (40 years).
+3.	Add a Method:
+o	Add a method named GetDetails that displays a message containing the student’s name, age, and ID.
+4.	Create Objects:
+o	Create an object from the Student class with the following values:
+	Name: "Ahmad"
+	Age: 22
+	Student ID: 1001
+o	Call the GetDetails method for the object.
+________________________________________
+ Add these Concepts
+1.	Add a Constructor:
+o	Add a constructor to the Student class that:
+	Accepts the name, age, and student ID as parameters.
+	Updates the class fields with the given values.
+2.	Add a Destructor:
+o	Add a destructor to the Student class that prints a message when the object is deleted.
+3.	Implement Encapsulation:
+o	Make all fields in the Student class private. (if we have field )
+o	Add Getters and Setters to control access to these fields.
+4.	The Four OOP Principles:
+o	Demonstrate Encapsulation by making student data private and using Getters and Setters.
+________________________________________
+Additional Instructions:
+1.	Examples:
+o	Create two new objects of the Student class.
+o	Display their details using the GetDetails method.
+2.	Code Comments:
+o	Add comments to the code to explain each step.
+*/
+using System;
+
+namespace task4
+{
+    internal class Program
+    {
+
+        class Student  // created a new class 
+        {
+            public const int MinAge = 18; // created a constant to hold the value of the minimum age of the students 
+            public const int MaxAge = 40; // created a constant to hold the value of the maximum age of the students 
+            public string Name { get; set; } // created auto property using set and get which resmbels the encapsulation. 
+
+            private int age;
+            public int Age
+            {
+                get
+                {
+                    return age;
+
+                }
+                set
+                {
+                    if (value < 18 || value > 40)
+                    {
+                        age = 18;
+                    }
+                    else
+                    {
+                        age = value;
+                    }
+                }
+            }// created auto property using set and get which resmbels the encapsulation. 
+            public int studentId { get; set; } // created auto property using set and get which resmbels the encapsulation. 
+            public string Email { get; set; } // created auto property using set and get which resmbels the encapsulation. 
+
+            public Student() // created a default constructor to give intial values to the proparties 
+            {
+                Name = string.Empty; // gave intial value for Name property
+                Age = 0; // gave intial value for Age property
+                studentId = 0; // gave intial value for studentId property
+            }
+            public void GetDetails() // created a method 
+            {
+                Console.WriteLine($"student’s name: {Name}\n age: {Age}\n ID: {studentId}"); // printed the vlaues of the propties using interpolation inside the method
+            }
+            public Student(string Name, int _Age, int studentId) // created a constructor that accepts 3 parameters 
+            {
+                this.Name = Name; // set the value of each property to equal the value that i will send in the parameters of the created objects that has parameters
+                this.Age = Age; // same as above 
+
+                if (_Age < 18 || _Age > 40)
+                {
+                    this.Age = 18;
+                }
+                else
+                {
+                    this.Age = _Age;
+                }
+                this.studentId = studentId; // same as above 
+
+            }
+            ~Student() { Console.WriteLine("You are done!"); } // created a destructor to delete all the cached data that were used in the objects
+        }
+
+        static void Main(string[] args)
+        {
+            Student student = new Student(); // created an obj of the class Student that is named student 
+            student.Name = "Ahmad"; // updated the intial value of the student(obj) name 
+            student.Age = 22; // updated the intial value of the student(obj) Age
+            student.studentId = 1001; // updated the intial value of the student(obj) studentId
+            student.GetDetails();
+            Student student1 = new Student("suhaib", 27, 9); // created an obj and sent values with it 
+            student1.GetDetails(); // called the method 
+            Student student2 = new Student("Adnan", 13, 8); // created an obj and sent values with it 
+            student2.GetDetails();
+
+        }
+    }
+}
